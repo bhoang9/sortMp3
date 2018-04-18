e64762715c8b27d89f8ac84a4c407d61ad1d996c
Brian H <brianbhoang9@gmail.com>
2018-04-18 00:22:29 -0700

    dealt w/ files with empty tags

--- sortMp3.py
+++ sortMp3.py
@@ -2,7 +2,7 @@
 #TODO: in cases where mp3 file may not have metadata, somehow
 #      get that metadata from the internet and assign it to that file
 
-import mutagen, os, magic
+import mutagen, os #, magic
 from mutagen.mp3 import EasyMP3
 from mutagen import MutagenError
 
@@ -17,8 +17,17 @@ print(files)
 for mp3_file in files:
     if(mp3_file.endswith('.mp3')):
         print(mp3_file)
-        audio = mutagen.File(mp3_file, None, True)
-        print(audio.tags)
+        try:
+            audio = mutagen.File(mp3_file, None, True)
+        except MutagenError:
+            print("Failed to load file")
+            audio.tags = []         #if file could not be loaded, wipe previous
+                                    #audio tags
+
+        if not audio.tags:          #if audio tags are empty
+            print("empty tags \n")
+        else:
+            print(audio.tags)
         print("\n") 
         #print(audio.tags['title'])
         #print(audio.tags['artist'])
