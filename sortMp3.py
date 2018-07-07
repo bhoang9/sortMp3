7c2eb9fcea2ea56386ada8ef8bdc5efd93a41674
Brian H <brianbhoang9@gmail.com>
2018-07-06 18:50:06 -0700

    comparing file name and metadata

--- sortMp3.py
+++ sortMp3.py
@@ -4,7 +4,7 @@
 #GOAL:      Need to decide on how to determine how and when to search
 #           database for information
 
-#BUG: currently EasyID3 is not airtight in determining when it has enough data for lookup 
+#CURRENT: able to look at tags and determine if artist or title tag is found in the filename
 
 import musicbrainzngs
 import mutagen, os
@@ -16,7 +16,7 @@ from mutagen import MutagenError
 musicbrainzngs.set_useragent("sortMp3", "0.1")
 
 #get the current directory's files
-os.chdir(os.getcwd() + "/music_samples")
+os.chdir(os.getcwd() + "/music_samples_small")
 files = os.listdir(os.getcwd())
 
 #print(files)
@@ -34,20 +34,27 @@ for mp3_file in files:
             #audio.tags = []         #if file could not be loaded, wipe previous
                                     #audio tags
 
-        #if not audio.tags:
-        #   print("empty tags \n")
-        if not audio:
+        #check if metadata was acquired
+        if not audio:               #sometimes mutagen generates an empty dictionary
             print("empty tags \n")
-        elif not audio['title']:
+        elif 'title' not in audio: 
+            print("empty tags \n")  #covers case where tags are not empty, but 'title' is
+        elif 'artist' not in audio: #case where tags not empty, but 'artist' is
             print("empty tags \n")
-        elif not audio['artist']:
-            print("empty tags \n")
+
+        #case that valid metadata was found
         else:
-            #if 'title' not in audio.tags:
-            #    print("no title tag")
-            #else:
-                #songDataTest(audio.tags['title'])
-            songDataTest(audio['title'])
+            #print(audio)
+            
+            #modifying file name and metadata strings to better look for substring
+            fileName = mp3_file.replace(".mp3", "").replace(" ", "").lower() #can be done w/ regex, maybe later
+            metadataTitle = audio['title'][0].replace(" ","").lower()
+            metadataArtist = audio['artist'][0].replace(" ","").lower()
+            #if metadata string is found in the file name, we know that the metadata is correct
+            if (metadataTitle in fileName or metadataArtist in fileName):
+                print("Song: " + audio['title'][0] + " | Artist: " + audio['artist'][0])
+                
+            #songDataTest(audio['title'])
 
 #        if not audio.tags:          #if audio tags are empty
 #            print("empty tags \n")
