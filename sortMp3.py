b15147c0460ad33225c8e57916dbdd3650ce6c6b
Brian <brianbhoang9@gmail.com>
2018-07-08 20:04:39 -0700

    pivot. split sorting and tagging into 2 programs

--- sortMp3.py
+++ sortMp3.py
@@ -1,13 +1,11 @@
-#Currently: able to run through files and acquire song name and artist
-#           able to acquire metadata from an internet database
+#CURRENT: able to naively check for correct file name format and create a new
+#         folder with the artist based on that simple formatting
 
-#GOAL:      Need to decide on how to determine how and when to search
-#           database for information
-
-#CURRENT: able to look at tags and determine if artist or title tag is found in the filename
+#GOAL: rename files if they are in the wrong format 
 
 import musicbrainzngs
 import mutagen, os
+import pathlib, shutil
 from tagSearch import songDataTest
 from mutagen.mp3 import EasyMP3
 from mutagen.easyid3 import EasyID3
@@ -16,58 +14,21 @@ from mutagen import MutagenError
 musicbrainzngs.set_useragent("sortMp3", "0.1")
 
 #get the current directory's files
-os.chdir(os.getcwd() + "/music_samples_small")
+os.chdir(os.getcwd() + "/music_samples_testing")
 files = os.listdir(os.getcwd())
+artists = []
 
 #print(files)
 
 #get audio file metadata
 for mp3_file in files:
-    if(mp3_file.endswith('.mp3')): 
+    if((mp3_file.endswith('.mp3')) and (" - " in mp3_file)):
+        artist = mp3_file.split(" - ", 1)[0]
         print(mp3_file)
-        try:
-            #audio = mutagen.File(mp3_file, None, True)
-            audio = EasyID3(mp3_file)
-        except MutagenError:
-            print("Failed to load file")
-            continue
-            #audio.tags = []         #if file could not be loaded, wipe previous
-                                    #audio tags
+        print("artist: " + artist + "\n")
 
-        #check if metadata was acquired
-        if not audio:               #sometimes mutagen generates an empty dictionary
-            print("empty tags \n")
-        elif 'title' not in audio: 
-            print("empty tags \n")  #covers case where tags are not empty, but 'title' is
-        elif 'artist' not in audio: #case where tags not empty, but 'artist' is
-            print("empty tags \n")
-
-        #case that valid metadata was found
-        else:
-            #print(audio)
-            
-            #modifying file name and metadata strings to better look for substring
-            fileName = mp3_file.replace(".mp3", "").replace(" ", "").lower() #can be done w/ regex, maybe later
-            metadataTitle = audio['title'][0].replace(" ","").lower()
-            metadataArtist = audio['artist'][0].replace(" ","").lower()
-            #if metadata string is found in the file name, we know that the metadata is correct
-            if (metadataTitle in fileName or metadataArtist in fileName):
-                print("Song: " + audio['title'][0] + " | Artist: " + audio['artist'][0])
-                
-            #songDataTest(audio['title'])
-
-#        if not audio.tags:          #if audio tags are empty
-#            print("empty tags \n")
-#        else:
-            #print(audio.tags)
-#            if 'title' not in audio.tags:
-#                print("empty title")
-#            else:
-#                print(audio.tags['title'])
-#            if 'artist' not in audio.tags:
-#                print("empty artist")
-#            else:
-#                print(audio.tags['artist'])
-        print("\n") 
-
+        #if correct file format, create new folder
+        #library creates new folder, handles if folder already exists
+        pathlib.Path(artist).mkdir(parents=True,exist_ok=True)
+        shutil.move(mp3_file, artist+"/"+mp3_file) #move file to new folder
 
